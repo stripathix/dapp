26d0c12bd654c259a677569225dba73529579a89
noob7 <qalvi.aupp@gmail.com>
2019-03-10 08:23:49 +0530

    Final code

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require("mongoose");
+var bodyParser = require('body-parser')
 const Web3 = require('web3');
 const TruffleContract = require('truffle-contract');
 const web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
@@ -12,10 +14,23 @@ const web3 = new Web3(web3Provider);
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+mongoose.connect("mongodb://127.0.0.1:27017");
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+    name:  String,
+    aadhar: String,
+    etheriumaddress: String
+  });
+const Account = mongoose.model('Account', accountSchema);
 
 app.use(express.static('client'));
 app.use(express.static('build/contracts'));
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 
 
@@ -32,13 +47,36 @@ app.get('/accounts', (req, res) => {
     });
 });
 
+app.post('/addaccount', (req, res) => {
+    console.log(req.body);
+    if (req.body.aadhar && req.body.etheriumaddress) {
+            var userAccount = new Account(req.body);
+            userAccount.save();
+            res.send("User addedd successfully");
+    } else {
+        res.status(413).send("Required paramaters missing");
+    }
+});
+app.get('/accountexist', (req, res) => {
+    if (req.query.aadhar) {
+            Account.findOne({ aadhar: req.query.aadhar }, function(err, character) {
+                if(!err) {
+                    res.send(character);
+                } else {
+                    res.send(err);
+                }
+              });
+    } else {
+        res.status(413).send("Required paramaters missing");
+    }
+});
 app.get('*', (req, res) => {
     res.status(404);
     res.send('Ooops... this URL does not exist');
 });
 
 app.listen(PORT, () => {
-    console.log(`TechBrij Ethereum HelloWorld App running on port ${PORT}...`);
+    console.log(`Ethereum running on port ${PORT}...`);
 });
 
 
